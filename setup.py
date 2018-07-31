4f1eafa723d0d96ac5d24592d77166acfdb55531
billli <240880759@qq.com>
2018-07-31 14:21:13 +0800

    1.setup改为读取配置 2.功能修改

--- setup.py
+++ setup.py
@@ -4,23 +4,28 @@
 # create on 2018/7/29
 
 # -*- coding:utf-8 -*-
+import ConfigParser
 import os, time
 from appium import webdriver
 
 # PATH = lambda p: os.path.abspath(
 #     os.path.join(os.path.dirname(__file__), p)
 # )
-
+# 读取配置文件
+conf = ConfigParser.SafeConfigParser()
+conf.read("phone.ini")
 desired_caps = {}
-desired_caps['platformName'] = 'Android'
+desired_caps['platformName'] = conf.get("samsung", "platformName")
 # 当前设备的版本号
-desired_caps['platformVersion'] = '7.0'
+desired_caps['platformVersion'] = conf.get("samsung", "platformVersion")
 # 设备名可以通过 adb devices 命令来获取
-desired_caps['deviceName'] = '9887a8353248344554'
+desired_caps['deviceName'] = conf.get("samsung", "deviceName")
 # 需要调试的App 的包名
-desired_caps['appPackage'] = 'com.qianjinjin.android'
+desired_caps['appPackage'] = conf.get("samsung", "appPackage")
 # 该 App 的启动 Activity
-desired_caps['appActivity'] = 'com.haohan.android.qdd.ui.activity.SplashActivity'
+desired_caps['appActivity'] = conf.get("samsung", "appActivity")
+# Don't reset app state before this session.
+desired_caps['noReset'] = conf.get("samsung", "noReset")
 # 若端口未变，则为固定代码
-driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+driver = webdriver.Remote(conf.get("samsung", "driverurl"), desired_caps)
 time.sleep(3)
